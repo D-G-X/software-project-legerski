7675fb83f0b8aa19bc4c6039e820c8dd61f2c552
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-11-06 14:39:56 +0100

    remove frontend project

--- hrlp-frontend/src/components/Navbar/Navbar.tsx
+++
@@ -1,6 +0,0 @@
-import {type JSX} from 'react'
-import "./Navbar.css";
-
-export default function Home(): JSX.Element | null {
-    return null
-}
--- hrlp-frontend/src/pages/Home/Home.tsx
+++
@@ -1,6 +0,0 @@
-import {type JSX} from 'react'
-import '/src/App.css'
-
-export default function Home(): JSX.Element | null {
-    return null
-}
