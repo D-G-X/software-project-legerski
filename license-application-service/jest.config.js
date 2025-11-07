2a3b1c816a086f4439740ba1b1373d9a697443df
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-11-07 22:27:17 +0100

    add css mocking to fix jest css err

--- license-application-service/jest.config.js
+++ license-application-service/jest.config.js
@@ -10,5 +10,9 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src/main/webapp'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '^react-i18next$': '<rootDir>/__mocks__/react-i18next.js',
+    },
 };
