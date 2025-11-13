f0310309f4d555f724354a3b913234a88387c85f
Darren Gonsalves <darrengons3001@gmail.com>
2025-11-13 09:34:15 +0100

    Applied the new config for the home page

--- license-application-service/src/main/webapp/app/home/home.tsx
+++ license-application-service/src/main/webapp/app/home/home.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import useDocumentTitle from 'app/common/use-document-title';
-import './home.css';
-
+import React from "react";
+import { useTranslation } from "react-i18next";
+import useDocumentTitle from "app/common/use-document-title";
+import "./home.css";
 
 export default function Home() {
   const { t } = useTranslation();
-  useDocumentTitle(t('home.index.headline'));
+  useDocumentTitle(t("home.index.headline"));
 
-  return (<>
-      <div className="relative min-h-[calc(100vh-4rem)]">
-          <h1
-              className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
-                   text-[256px] font-extrabold text-center"
-              style={{ color: '#351341' }}
-          >
-              {t('home.index.headline')}
-          </h1>
+  return (
+    <main className="relative h-screen w-full overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-top bg-no-repeat blur-[2px] opacity-80 scale-105"
+        style={{ backgroundImage: "url('/images/background.jpg')" }}
+      ></div>
+
+      <div className="relative min-h-[calc(100vh-4rem)] flex items-center justify-center">
+        <h1 className="text-[256px] font-extrabold text-center text-mallorca-purple z-10">
+          {t("home.index.headline")}
+        </h1>
       </div>
-  </>);
+    </main>
+  );
 }
