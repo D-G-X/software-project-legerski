d484897d2ebb81ebf8ad8bc98fd427d25380cbff
Darren Gonsalves <darrengons3001@gmail.com>
2025-11-14 06:51:51 +0100

    Header Changes, Home Page changes

--- license-application-service/src/main/webapp/app/common/header.tsx
+++ license-application-service/src/main/webapp/app/common/header.tsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
-import { ChevronDown } from "lucide-react";
+// import { ChevronDown } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { LanguageInfo } from "./utils";
 
 export default function Header() {
   const { t, i18n } = useTranslation();
   const [open, setOpen] = useState(false);
+
+  const language_title_img_map: Record<string, LanguageInfo> = {
+    en: {
+      language: "English",
+      img: "/images/languages/english.png",
+    },
+    es: {
+      language: "Spanish",
+      img: "/images/languages/spanish.png",
+    },
+  };
+
+  const totalItems = Object.keys(language_title_img_map).length;
 
   const toggleDropdown = () => setOpen((prev) => !prev);
   const changeLanguage = (lng: string) => {
@@ -44,8 +58,8 @@ export default function Header() {
   }, []);
 
   return (
-    <header ref={headerRef} className="bg-gray-50 h-16">
-      <nav className="flex py-2 w-full px-4 md:px-10">
+    <header ref={headerRef} className="bg-gray-5 max-h-20 font-inter">
+      <nav className="py-4 w-full px-5 md:px-10">
         <div className="flex justify-between w-full">
           <div>
             <Link
@@ -59,14 +73,85 @@ export default function Header() {
                 height="100"
                 className="inline-block"
               />
-              <span className="text-2xl pl-5 font-semibold text-mallorca-purple">
+              <span className="text-xl md:text-2xl pl-5 font-semibold text-mallorca-purple">
                 {t("app.title")}
               </span>
             </Link>
           </div>
           <div>
+            {/* Language Dropdown */}
+            <div
+              className="flex md:block grow md:grow-0 justify-end basis-full md:basis-auto pt-3 md:pt-1 pb-1"
+              id="navbarToggle"
+            >
+              <ul className="flex">
+                <li className="relative group">
+                  <button
+                    type="button"
+                    onClick={toggleDropdown}
+                    className="text-gray-500 p-2 cursor-pointer flex items-center"
+                  >
+                    <div className="flex mr-2">
+                      <span className="mx-3 text-lg text-mallorca-purple/75">
+                        {language_title_img_map[i18n.language]?.language}
+                      </span>
+                      <span>
+                        <img
+                          src={language_title_img_map[i18n.language]?.img}
+                          alt={t("app.title")}
+                          className="inline-block h-5 w-8"
+                        />
+                      </span>
+                    </div>
+                    {/* <ChevronDown
+                      className={`w-4 h-4 transition-transform duration-200 ${
+                        open ? "rotate-180" : "rotate-0"
+                      }`}
+                    /> */}
+                  </button>
+                  {open && (
+                    <ul className="absolute right-0 bg-white border-gray-300 shadow-sm shadow-black rounded z-50 min-w-40">
+                      {Object.keys(language_title_img_map).map((key, index) => {
+                        const lang = language_title_img_map[key];
+                        const isSelected = i18n.language === key;
+                        const roundedClass =
+                          index === 0
+                            ? "rounded-t-md"
+                            : index === totalItems - 1
+                            ? "rounded-b-md"
+                            : "";
+
+                        return (
+                          <li
+                            key={key}
+                            className={`${
+                              isSelected ? "bg-gray-200" : "bg-white"
+                            } ${roundedClass}`}
+                          >
+                            <button
+                              onClick={() => changeLanguage(key)}
+                              className={`flex justify-between items-center gap-2 w-full text-mallorca-purple/75 hover:bg-gray-100 px-4 py-2 ${roundedClass}`}
+                            >
+                              <div className="text-lg text-left">
+                                {lang?.language}
+                              </div>
+                              <img
+                                src={lang?.img}
+                                alt={t("app.title")}
+                                className="inline-block h-5 w-8"
+                              />
+                            </button>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            </div>
+
             {/* Hamburger Menu */}
-            <div className="">
+            {/* <div className="">
               <button
                 type="button"
                 className="js-dropdown md:hidden rounded cursor-pointer"
@@ -81,59 +166,7 @@ export default function Header() {
                   <div className="w-6 h-0.5 bg-gray-500"></div>
                 </div>
               </button>
-            </div>
-            {/* Language Dropdown */}
-            <div
-              className="flex md:block grow md:grow-0 justify-end basis-full md:basis-auto pt-3 md:pt-1 pb-1 border"
-              id="navbarToggle"
-            >
-              <ul className="flex">
-                <li className="relative group">
-                  <button
-                    type="button"
-                    onClick={toggleDropdown}
-                    className="text-gray-500 p-2 cursor-pointer flex items-center"
-                  >
-                    <span className="mr-1">{i18n.language.toUpperCase()}</span>
-                    <ChevronDown
-                      className={`w-4 h-4 transition-transform duration-200 ${
-                        open ? "rotate-180" : "rotate-0"
-                      }`}
-                    />
-                  </button>
-                  {open && (
-                    <ul className="hidden group-hover:block absolute right-0 bg-white border border-gray-300 rounded min-w-24 py-2 z-50">
-                      <li>
-                        <button
-                          onClick={() => open && changeLanguage("en")}
-                          className="block w-full text-left px-4 py-1 hover:bg-gray-200"
-                        >
-                          <img
-                            src="/images/languages/english.png"
-                            alt={t("app.title")}
-                            width="50"
-                            className="inline-block"
-                          />
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          onClick={() => open && changeLanguage("es")}
-                          className="block w-full text-left px-4 py-1 hover:bg-gray-200"
-                        >
-                          <img
-                            src="/images/languages/spanish.png"
-                            alt={t("app.title")}
-                            width="50"
-                            className="inline-block"
-                          />
-                        </button>
-                      </li>
-                    </ul>
-                  )}
-                </li>
-              </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </nav>
