208868d0f440a9fc4a9dd5672ccdb72b2971d089
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-11-16 01:59:21 +0100

    improve validation rules for user name, email & password
    Name: add french and german characters (not just  [A-Za-z]
    Email: add RFC 5322 Format
    Password: add passphrase support: length >= 8, extended set of special characters and remove neccesity for uppercase and numbers
    
    And move validateConfirmPassword function to password

--- license-application-service/src/main/webapp/app/common/validationRules.ts
+++ license-application-service/src/main/webapp/app/common/validationRules.ts
@@ -4,7 +4,10 @@ import { t } from "i18next";
 export const validateName = (value: string) => {
   if (!value) return { isValid: false, message: t("validation.name.required") };
 
-  const nameRegex = /^[A-Za-z\s]+$/;
+  const nameRegex = new RegExp(
+    "^[\\p{L}\\p{M}'’-]+(?: [\\p{L}\\p{M}'’-]+)*$",
+    "u"
+  );
 
   if (!nameRegex.test(value))
     return {
@@ -21,66 +24,54 @@ export const validateName = (value: string) => {
   return { isValid: true, message: t("validation.name.valid") };
 };
 
-// Confirm Password validation
-export const validateConfirmPassword = (
-  password: string,
-  confirmPassword: string
-) => {
-  if (!confirmPassword)
-    return {
-      isValid: false,
-      message: t("validation.confirmPassword.required"),
-    };
-
-  if (password !== confirmPassword)
-    return {
-      isValid: false,
-      message: t("validation.confirmPassword.mismatch"),
-    };
-
-  return { isValid: true, message: t("validation.confirmPassword.valid") };
-};
-
 // Email validation
 export const validateEmail = (value: string) => {
   if (!value)
     return { isValid: false, message: t("validation.email.required") };
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  const emailRegex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
   return emailRegex.test(value)
     ? { isValid: true, message: t("validation.email.valid") }
     : { isValid: false, message: t("validation.email.invalid") };
 };
 
-// Password validation
+// Password validation: at least 8 characters long and at least one special character
 export const validatePassword = (value: string) => {
   if (!value)
     return { isValid: false, message: t("validation.password.required") };
 
-  if (value.length < 6)
+  if (value.length < 8)
     return {
       isValid: false,
       message: t("validation.password.minLength"),
     };
 
-  if (!/[A-Z]/.test(value))
-    return {
-      isValid: false,
-      message: t("validation.password.uppercase"),
-    };
-
-  if (!/[0-9]/.test(value))
-    return {
-      isValid: false,
-      message: t("validation.password.number"),
-    };
-
-  if (!/[!@#$%^&*]/.test(value))
+  // At least one special character (non letter or number)
+  if (!/[\p{P}\p{S}]/u.test(value))
     return {
       isValid: false,
       message: t("validation.password.specialChar"),
     };
 
   return { isValid: true, message: t("validation.password.valid") };
+};
+
+// Confirm Password validation
+export const validateConfirmPassword = (
+    password: string,
+    confirmPassword: string
+) => {
+    if (!confirmPassword)
+        return {
+            isValid: false,
+            message: t("validation.confirmPassword.required"),
+        };
+
+    if (password !== confirmPassword)
+        return {
+            isValid: false,
+            message: t("validation.confirmPassword.mismatch"),
+        };
+
+    return { isValid: true, message: t("validation.confirmPassword.valid") };
 };
