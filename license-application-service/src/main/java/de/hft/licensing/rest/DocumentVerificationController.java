a3d0484af86b6a967d9368367e0e3da6da264999
Florian Vasica <12vafl1bif@hft-stuttgart.de>
2025-11-28 17:53:02 +0100

    Finalized DocumentVerificationController, yet to be implemented

--- license-application-service/src/main/java/de/hft/licensing/rest/DocumentVerificationController.java
+++ license-application-service/src/main/java/de/hft/licensing/rest/DocumentVerificationController.java
@@ -3,6 +3,7 @@ package de.hft.licensing.rest;
 import de.hft.licensing.api.DocumentVerificationApi;
 import de.hft.licensing.model.ApplicationDocumentResource;
 import de.hft.licensing.model.VerifyDocumentRequest;
+import org.jooq.DSLContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +12,13 @@ import java.util.List;
 
 @RestController
 public class DocumentVerificationController implements DocumentVerificationApi {
+
+    private final DSLContext dsl;
+
+    public DocumentVerificationController(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
     @Override
     public ResponseEntity<List<ApplicationDocumentResource>> listDocuments(Integer applicationId) {
         return null;
