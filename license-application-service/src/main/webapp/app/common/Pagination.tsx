510b45b48b6c2ea1c47d4bdde449efed0b72de2b
sahmed <s.ahmed.the1st@gmail.com>
2025-11-30 01:23:37 +0100

    Dashboard completed
    Dashboard work is completed but may neeed to work on some aspects concerned with backend API

--- license-application-service/src/main/webapp/app/common/Pagination.tsx
+++ license-application-service/src/main/webapp/app/common/Pagination.tsx
@@ -5,25 +5,39 @@ interface PaginationProps {
     currentPage: number;
     totalPage: number;
     onPageChange: (page: number) => void; // Defines a function that takes a number and returns nothing (void)
+    start: number;
+    end: number;
+    numberOfItems: number
 }
 
 // 2. Attach the interface to the component function
-const Pagination: React.FC<PaginationProps> = ({totalPage, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({currentPage, totalPage, onPageChange, start, end, numberOfItems
+}) => {
     // The rest of the logic is correct JSX
     const pages = Array.from({ length: totalPage }, (_, i) => i + 1);
 
     return (
-        <div>
-            {pages.map((page) => (
-                // Key prop is correct, and the onClick handler is correct
-                <button
-                    key={page}
-                    onClick={ () => onPageChange(page)}
-                    // You might add a className here to style the current page
-                >
-                    {page}
+        <div className={"flex justify-between mt-4 items-center "}>
+            <div className={"text-xs text-gray-500"}>
+                Showing from from {start} to {end > numberOfItems ? numberOfItems : end} of {numberOfItems}
+            </div>
+            <div>
+                <button className="mx-1 px-3 py-1 bg-gray-200 text-black rounded-sm disabled:opacity-50"
+                        onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>Prev
                 </button>
-            ))}
+                {pages.map((page) => (
+                    // Key prop is correct, and the onClick handler is correct
+                    <button
+                        className={`mx-1 px-3 py-1 ${currentPage === page ? 'bg-blue-600' : 'bg-gray-200'} ${currentPage === page ? 'text-white' : 'text-black'} rounded-sm disabled:opacity-50`}
+                        key={page}
+                        onClick={() => onPageChange(page)}
+                        // You might add a className here to style the current page
+                    >
+                        {page}
+                    </button>
+                ))}
+                <button className="mx-1 px-3 py-1 bg-gray-200 text-black rounded-sm disabled:opacity-50" onClick={ () => onPageChange(currentPage + 1)} disabled={currentPage === totalPage}>Next</button>
+            </div>
         </div>
     );
 };
--- license-application-service/src/main/webapp/app/dashboard/dashboard.tsx
+++ license-application-service/src/main/webapp/app/dashboard/dashboard.tsx
@@ -4,11 +4,14 @@ import {useTranslation} from "react-i18next";
 import useDocumentTitle from "../common/use-document-title";
 import "./dashboard.css";
 import Pagination from "../common/Pagination";
+import {Link} from "react-router";
 // import useDocumentTitle from "app/common/use-document-title";
 
 
 
 export default function Dashboard(){
+
+    const { t } = useTranslation();
 
     const licenseRecords = [
         {
@@ -16,70 +19,70 @@ export default function Dashboard(){
             "address": "In der Au 16B, Stuttgart",
             "requested_date": "2025-07-01T10:00:00Z",
             "license_type": "ETV",
-            "license_status": "ACTIVE",
+            "license_status": "Accepted",
         },
         {
             "application_id": 202,
             "address": "Königstraße 54, Stuttgart",
             "requested_date": "2025-07-01T14:30:00Z",
             "license_type": "B2C",
-            "license_status": "PENDING",
+            "license_status": "Declined",
         },
         {
             "application_id": 203,
             "address": "Hauptstraße 88, Ludwigsburg",
             "requested_date": "2025-07-02T09:15:00Z",
             "license_type": "ETV",
-            "license_status": "REVOKED",
+            "license_status": "In Process",
         },
         {
             "application_id": 204,
             "address": "Filderbahnplatz 28, Stuttgart",
             "requested_date": "2025-07-02T16:00:00Z",
             "license_type": "B2C",
-            "license_status": "ACTIVE",
+            "license_status": "Draft",
         },
         {
             "application_id": 205,
             "address": "Gerberstraße 12, Esslingen am Neckar",
             "requested_date": "2025-07-03T11:20:00Z",
             "license_type": "HML",
-            "license_status": "PENDING",
+            "license_status": "Expired",
         },
         {
             "application_id": 206,
             "address": "Charlottenplatz 1, Stuttgart",
             "requested_date": "2025-07-03T15:45:00Z",
             "license_type": "ETV",
-            "license_status": "ACTIVE",
+            "license_status": "Accepted",
         },
         {
             "application_id": 207,
             "address": "Schillerplatz 7, Waiblingen",
             "requested_date": "2025-07-04T08:00:00Z",
             "license_type": "B2C",
-            "license_status": "ARCHIVED",
+            "license_status": "Expired",
         },
         {
             "application_id": 208,
             "address": "Theodor-Heuss-Straße 30, Stuttgart",
             "requested_date": "2025-07-04T12:00:00Z",
             "license_type": "HML",
-            "license_status": "PENDING",
+            "license_status": "Draft",
         },
         {
             "application_id": 209,
             "address": "Marktplatz 1, Fellbach",
             "requested_date": "2025-07-05T13:30:00Z",
             "license_type": "ETV",
-            "license_status": "ACTIVE",
+            "license_status": "Expired",
         },
         {
             "application_id": 210,
             "address": "Rotebühlplatz 2, Stuttgart",
             "requested_date": "2025-07-05T17:00:00Z",
             "license_type": "B2C",
-            "license_status": "REVOKED",
+            "license_status": "Declined",
         }
     ];
 
@@ -90,29 +93,26 @@ export default function Dashboard(){
     const totalPage = Math.ceil(licenseRecords.length / itepmsPerPage); // Determine total number of pages
     const currentData = licenseRecords.slice((currentPage - 1) * itepmsPerPage, currentPage * itepmsPerPage); // Get current page data
 
-    const { t } = useTranslation();
-    useDocumentTitle(t("home.index.headline"));
+        useDocumentTitle(t("home.index.headline"));
 
     return (
         <div className="container mx-auto px-4 md:px-6">
-            <div className="justify-center mt-8">
-                <h2 className="font-bold text-center mb-6">{t("dashboard.headline")}</h2>
+            <div className="relative min-h-[calc(100vh-4rem)] bg-white justify-center mt-8">
+                <h2 className="font-bold text-center text-xl mb-6">{t("dashboard.headline")}</h2>
                 <button
                     type="button"
                     className="bg-mallorca-purple text-white px-10 py-2 rounded-md w-full font-medium text-lg mb-6 w-max block m-auto">
                     {t("dashboard.button-text")}
                 </button>
-            </div>
-            <div>
-                <h1 className="font-bold">{t("dashboard.title")}</h1>
-                <table>
+                <h1 className="font-bold text-xl">{t("dashboard.title")}</h1>
+                <table className={"mt-8"}>
                     <tr>
-                        <th>Application ID</th>
-                        <th>Address</th>
-                        <th>Request Date</th>
-                        <th>Licence Type</th>
-                        <th>Status</th>
-                        <th>Action</th>
+                        <th>{t("dashboard.table.application_id")}</th>
+                        <th>{t("dashboard.table.address")}</th>
+                        <th>{t("dashboard.table.request_date")}</th>
+                        <th>{t("dashboard.table.licence_type")}</th>
+                        <th>{t("dashboard.table.status")}</th>
+                        <th>{t("dashboard.table.action.title")}</th>
                     </tr>
                     {currentData.map(item => (
                         <tr key={item.application_id}>
@@ -120,8 +120,17 @@ export default function Dashboard(){
                             <td>{item.address}</td>
                             <td>{item.requested_date}</td>
                             <td>{item.license_type}</td>
-                            <td>{item.license_status}</td>
-                            <td>More Action</td>
+                            <td>
+                                <span className={`text-center p-1 px-4 border rounded-md ${item.license_status === 'Accepted' ? "text-green-600 bg-green-300 border-green-600" :
+                                    (item.license_status === "Declined" ? "text-red-600 bg-red-300 border-red-600" :
+                                    (item.license_status === "In Process" ? "text-orange-600 bg-orange-300 border-orange-600" :
+                                    (item.license_status === "Draft" ? "text-gray-600 bg-gray-300 border-gray-600" : "text-green-600 bg-red-300 border-green-600" )))
+                                }`}>
+                                    {item.license_status}
+                                </span>
+
+                            </td>
+                            <td><Link to="register" className={"text-blue-500 underline"}>{t("dashboard.table.action.value")}</Link></td>
                         </tr>
                     ))}
                 </table>
@@ -130,11 +139,11 @@ export default function Dashboard(){
                     currentPage={currentPage}
                     totalPage={totalPage}
                     onPageChange={setCurrentPage}
+                    start={((currentPage - 1) * itepmsPerPage) + 1}
+                    end={currentPage * itepmsPerPage}
+                    numberOfItems={licenseRecords.length}
                 />
             </div>
-
-
-
         </div>
     );
 }
