bf92dba6aacb4c190a19fceefbb16d6b284fdbb3
Darren Gonsalves <darrengons3001@gmail.com>
2025-12-01 09:59:31 +0100

    Removed unwanted dashboard URL

--- license-application-service/src/main/webapp/app/routes.tsx
+++ license-application-service/src/main/webapp/app/routes.tsx
@@ -8,7 +8,6 @@ import Register from "./register/register";
 import ForgotPasswordRequest from "./forgot-password/request";
 import ResetPassword from "./forgot-password/reset";
 import Payment from "./payment/payment";
-import Dashboard from "./dashboard/dashboard";
 
 export default function AppRoutes() {
   const router = createBrowserRouter([
@@ -20,7 +19,6 @@ export default function AppRoutes() {
         { path: "reset-password", element: <ResetPassword /> },
         { path: "login", element: <Login /> },
         { path: "register", element: <Register /> },
-        { path: "applications", element: <Dashboard /> },
         { path: "payment", element: <Payment /> },
         { path: "error", element: <Error /> },
         { path: "*", element: <Error /> },
