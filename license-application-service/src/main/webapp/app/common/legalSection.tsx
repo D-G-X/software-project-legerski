387cfaa356d8c07b21afa4a7fc00b6379837b044
Darren Gonsalves <darrengons3001@gmail.com>
2025-12-03 16:33:56 +0100

    Legal page code refactored, Resolved bug for scroll, and added indentation in legal page for better readability for the user.

---
+++ license-application-service/src/main/webapp/app/common/legalSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface SectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Section: React.FC<SectionProps> = ({ title, children }) => {
+  return (
+    <section className="mt-5">
+      <h2 className="text-xl font-semibold text-mallorca-purple">{title}</h2>
+      <div className="pl-6 mt-2">{children}</div>
+    </section>
+  );
+};
+
+export const SubSection: React.FC<SectionProps> = ({ title, children }) => {
+  return (
+    <section className="mt-5">
+      <h3 className="text-lg font-semibold text-mallorca-purple">{title}</h3>
+      <div className="pl-9 mt-2">{children}</div>
+    </section>
+  );
+};
