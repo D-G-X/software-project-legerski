bf677cbb6c92a4a28622ea300d9a0968ecd84cd8
Darren Gonsalves <darrengons3001@gmail.com>
2025-12-03 23:29:11 +0100

    validator probability changed to 90%

--- license-application-mock-validator/document-validator.go
+++ license-application-mock-validator/document-validator.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-const verifiedProbability = 50 // percentage chance of VERIFIED status
+const verifiedProbability = 90 // percentage chance of VERIFIED status
 const maxFileSize = 10          // per file in MB
 const maxPollRequests = 2       // max polling requests per second per client
 const timeout = 5               // seconds
