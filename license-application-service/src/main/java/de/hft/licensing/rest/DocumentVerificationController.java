fdd04861fc21fb0061c0586db47e0bad590ab151
Florian Vasica <12vafl1bif@hft-stuttgart.de>
2025-12-04 16:23:46 +0100

    - set up authorization for DocumentVerificationController

--- license-application-service/src/main/java/de/hft/licensing/rest/DocumentVerificationController.java
+++ license-application-service/src/main/java/de/hft/licensing/rest/DocumentVerificationController.java
@@ -5,20 +5,25 @@ import de.hft.licensing.db.enums.VerificationStatus;
 import de.hft.licensing.db.tables.Application;
 import de.hft.licensing.db.tables.records.ApplicationRecord;
 import de.hft.licensing.model.DocumentValidationCallbackRequest;
+import de.hft.licensing.services.auth.ApplicationAuthorizationService;
 import org.jooq.DSLContext;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DocumentVerificationController implements DocumentVerificationApi {
 
     private final DSLContext dsl;
+    private final ApplicationAuthorizationService applicationAuthorization;
 
-    public DocumentVerificationController(DSLContext dsl) {
+    public DocumentVerificationController(DSLContext dsl, ApplicationAuthorizationService applicationAuthorization) {
         this.dsl = dsl;
+        this.applicationAuthorization = applicationAuthorization;
     }
 
     @Override
+    @PreAuthorize("hasRole('mock_validator')")
     public ResponseEntity<Void> documentValidationCallback(DocumentValidationCallbackRequest documentValidationCallbackRequest) {
         if (documentValidationCallbackRequest.getStatus() == DocumentValidationCallbackRequest.StatusEnum.VERIFIED) {
             var updated = dsl.update(Application.APPLICATION)
@@ -46,6 +51,7 @@ public class DocumentVerificationController implements DocumentVerificationApi {
     }
 
     @Override
+    @PreAuthorize("@applicationAuthorization.canAccessApplication(authentication, #applicationId)")
     public ResponseEntity<DocumentValidationCallbackRequest> getApplicationDocuments(Integer applicationId) {
         ApplicationRecord record = dsl.selectFrom(Application.APPLICATION)
                 .where(Application.APPLICATION.ID.eq(applicationId))
