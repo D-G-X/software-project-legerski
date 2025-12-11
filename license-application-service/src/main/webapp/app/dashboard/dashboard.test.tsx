fb7f61d164a365eec021d72b6fa1066e1f4d7463
Jay Italiya <88088549+JayItaliya09@users.noreply.github.com>
2025-12-11 03:42:53 +0100

    Added placeholder tests to the empty test files

---
+++ license-application-service/src/main/webapp/app/dashboard/dashboard.test.tsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from '@jest/globals';
+
+describe('Dashboard', () => {
+  it('should be implemented', () => {
+    //Add tests for dashboard component
+    expect(true).toBe(true);
+  });
+});
