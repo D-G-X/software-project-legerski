52a7ace205bc3feec025f0d8b5051d2347e3545d
Vahan Ducher <vahan.ducher@icloud.com>
2025-12-15 18:20:59 +0100

    edit: change endpoints for preferences getting and updating

--- license-application-service/src/main/java/de/hft/licensing/rest/UsersController.java
+++ license-application-service/src/main/java/de/hft/licensing/rest/UsersController.java
@@ -4,12 +4,9 @@ import de.hft.licensing.api.UsersApi;
 import de.hft.licensing.db.enums.NotificationWay;
 import de.hft.licensing.db.tables.NotificationPreferences;
 import de.hft.licensing.db.tables.User;
+import de.hft.licensing.db.tables.records.NotificationPreferencesRecord;
 import de.hft.licensing.db.tables.records.UserRecord;
-import de.hft.licensing.model.CreateUserRequest;
-import de.hft.licensing.model.NotificationWayApiEnum;
-import de.hft.licensing.model.UpdateUserRequest;
-import de.hft.licensing.model.UserNotificationResource;
-import de.hft.licensing.model.UserResource;
+import de.hft.licensing.model.*;
 import de.hft.licensing.services.KeycloakAuthService;
 import de.hft.licensing.services.auth.AdminOnly;
 import de.hft.licensing.utils.EnumMapperUtil;
@@ -17,6 +14,7 @@ import de.hft.licensing.utils.RecordToResourceMapperUtil;
 import org.jooq.DSLContext;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientResponseException;
 
@@ -218,5 +216,45 @@ public class UsersController implements UsersApi {
     @Override
     public ResponseEntity<Void> updateNotifications(Integer id) {
         return null;
+    }
+
+    @Override
+    @PreAuthorize("@preferencesAuthorization.canAccessPreferences(authentication, #userId)")
+    public ResponseEntity<NotificationPreferencesResource> getNotificationPreferences(UUID userId) {
+        var result = dsl.select()
+                .from(NotificationPreferences.NOTIFICATION_PREFERENCES)
+                .where(NotificationPreferences.NOTIFICATION_PREFERENCES.USER_ID.eq(userId.toString()))
+                .fetchOneInto(NotificationPreferencesRecord.class);
+
+        NotificationPreferencesResource apiResource = new NotificationPreferencesResource();
+        RecordToResourceMapperUtil.mapNotificationPreferencesRecordToResource(result, apiResource);
+
+        return result != null ?
+                ResponseEntity.ok(apiResource) :
+                ResponseEntity.notFound().build();
+    }
+
+    @Override
+    @PreAuthorize("@preferencesAuthorization.canAccessPreferences(authentication, #userId)")
+    public ResponseEntity<NotificationPreferencesResource> updateNotificationPreferences(UUID userId, NotificationPreferencesUpdate notificationPreferencesUpdate) {
+        if (userId == null || notificationPreferencesUpdate == null || notificationPreferencesUpdate.getNotificationWay() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var updatedRecord = dsl.update(NotificationPreferences.NOTIFICATION_PREFERENCES)
+                .set(NotificationPreferences.NOTIFICATION_PREFERENCES.NOTIFICATION_WAY, (NotificationWay) EnumMapperUtil.getPendantFromEnum(notificationPreferencesUpdate.getNotificationWay()))
+                .set(NotificationPreferences.NOTIFICATION_PREFERENCES.APPLICATION_UPDATES_NOTIFICATION, notificationPreferencesUpdate.getApplicationUpdatesNotification())
+                .set(NotificationPreferences.NOTIFICATION_PREFERENCES.LICENSE_RENEWAL_NOTIFICATION,  notificationPreferencesUpdate.getLicenseRenewalNotification())
+                .where(NotificationPreferences.NOTIFICATION_PREFERENCES.USER_ID.eq(userId.toString()))
+                .returning()
+                .fetchOneInto(NotificationPreferencesRecord.class);
+
+        if (updatedRecord != null) {
+            NotificationPreferencesResource apiResource = new NotificationPreferencesResource();
+            RecordToResourceMapperUtil.mapNotificationPreferencesRecordToResource(updatedRecord, apiResource);
+            return ResponseEntity.ok(apiResource);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
