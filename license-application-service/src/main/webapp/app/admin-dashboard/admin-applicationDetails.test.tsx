0b54b95a990dd16fd629ecfc6e7de227e6026c4b
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-12-15 16:57:46 +0100

    add basic admin application details

---
+++ license-application-service/src/main/webapp/app/admin-dashboard/admin-applicationDetails.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AdminApplicationDetails from "./admin-applicationDetails";
+
+test("should create", () => {
+  const { container } = renderWithRouter(
+      <AdminApplicationDetails
+          open={false}
+          applicationData={null}
+          onClose={jest.fn()}
+          onRenew={jest.fn()}
+      />
+  );
+
+  expect(container).toBeTruthy();
+});
+
+test("should render title when open", () => {
+  const { getByText } = renderWithRouter(
+      <AdminApplicationDetails
+          open={true}
+          applicationData={{ id: 1 } as any}
+          onClose={jest.fn()}
+          onRenew={jest.fn()}
+      />
+  );
+
+  expect(getByText(/application details/i)).toBeInTheDocument();
+});
