5f9c3a3eca1469fc2fece3d204cf7a0590be011a
Florian Vasica <12vafl1bif@hft-stuttgart.de>
2025-12-15 15:53:19 +0100

    - implementation of ballot logic

--- license-application-service/src/main/java/de/hft/licensing/rest/LicensesController.java
+++ license-application-service/src/main/java/de/hft/licensing/rest/LicensesController.java
@@ -1,7 +1,6 @@
 package de.hft.licensing.rest;
 
 import de.hft.licensing.api.LicensesApi;
-
 import de.hft.licensing.db.enums.LicenseStatus;
 import de.hft.licensing.db.tables.License;
 import de.hft.licensing.db.tables.records.LicenseRecord;
@@ -18,6 +17,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
