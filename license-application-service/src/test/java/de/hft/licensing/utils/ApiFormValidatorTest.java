79f12734c0dd1797e6a4270440ce7317db5ff191
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-12-19 20:45:24 +0100

    Update ApiFormValidatorTest.java

--- license-application-service/src/test/java/de/hft/licensing/utils/ApiFormValidatorTest.java
+++ license-application-service/src/test/java/de/hft/licensing/utils/ApiFormValidatorTest.java
@@ -1,9 +1,10 @@
 package de.hft.licensing.utils;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ApiFormValidatorTest {
 
@@ -16,9 +17,73 @@ public class ApiFormValidatorTest {
 
   @Nested
   class testIsValidName {
+
     @Test
     void returnsFalse_whenNameIsEmpty() {
       assertFalse(validator.isValidName(""));
+    }
+  }
+
+  @Nested
+  class testIsValidEmail {
+
+    @Test
+    void returnsFalse_whenEmailIsEmpty() {
+      assertFalse(validator.isValidEmail(""));
+    }
+  }
+
+  @Nested
+  class testIsValidCadastralNumber {
+
+    @Test
+    void returnsFalse_whenCadastralNumberIsEmpty() {
+      assertFalse(validator.isValidCadastralNumber(""));
+    }
+  }
+
+  @Nested
+  class testIsValidPassword {
+
+    @Test
+    void returnsFalse_whenPasswordIsEmpty() {
+      assertFalse(validator.isValidPassword(""));
+    }
+  }
+
+  @Nested
+  class testIsValidConfirmPassword {
+
+    @Test
+    void returnsFalse_whenConfirmPasswordAndPasswordIsEmpty() {
+      assertFalse(validator.isValidConfirmPassword("", ""));
+    }
+  }
+
+  @Nested
+  class testIsValidIban {
+
+    @Test
+    void returnsFalse_whenIbanIsEmpty() {
+      assertFalse(validator.isValidIban(""));
+    }
+  }
+
+  @Nested
+  class testIsValidBic {
+
+    @Test
+    void returnsFalse_whenBicAndIbanIsEmpty() {
+      assertFalse(validator.isValidBic("", ""));
+    }
+  }
+
+  @Nested
+  class testIsValidDateString {
+
+    @Test
+    void returnsFalse_whenDateStringIsEmpty() {
+      assertFalse(validator.isValidDateString(""));
     }
   }
 
