61d927cf4b5b04cdd95c32b7785a513bfb9805b8
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-12-21 02:47:50 +0100

    Update admin-applicationDetails.tsx

--- license-application-service/src/main/webapp/app/admin-dashboard/admin-applicationDetails.tsx
+++ license-application-service/src/main/webapp/app/admin-dashboard/admin-applicationDetails.tsx
@@ -8,7 +8,7 @@ import {
   LicenseResource,
 } from "../../types";
 import {Minimize2} from "lucide-react";
-import "./applicationDetails.css";
+import "./admin-applicationDetails.css";
 import {useListPayments} from "../services/payments/payments";
 import {useDeleteLicense, useGetLicense, useListLicenses} from "../services/licenses/licenses";
 import {useGetApplicationDocuments} from "../services/document-verification/document-verification";
