ec3b8d0bb9215cc628b164b23f90f4dda0a2e972
sahmed <s.ahmed.the1st@gmail.com>
2025-12-27 15:14:21 +0100

    Ballot applications page is created

---
+++ license-application-service/src/main/webapp/app/ballot-applications-dashboard/ballot-applications-dashboard.tsx
@@ -0,0 +1,375 @@
+import React, {useState} from "react";
+import { useTranslation } from "react-i18next";
+import useDocumentTitle from "../common/use-document-title";
+import "./ballot-applications-dashboard.css";
+import Pagination from "../common/Pagination";
+import { Link, useNavigate, useParams } from "react-router";
+// import { useListApplications } from "app/services/applications/applications";
+// import { ApplicationResource } from "../../types";
+
+export default function BallotApplicationsDashboard() {
+    const { ballotId } = useParams();
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    useDocumentTitle(t("home.index.headline"));
+
+
+
+
+
+
+
+    const formatDate = (rawDate: string | undefined) => {
+        if (!rawDate) return "";
+        return new Date(rawDate).toLocaleString(t("locale"), {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    };
+
+    function handleNewApplicationClick() {
+        navigate(`/login`);
+    }
+
+    const localData = [
+        {
+            "id": 1,
+            "ballot_id": "001",
+            "name": "Ahmed",
+            "address": "Home",
+            "phone_number": "111",
+            "email": "ahmed@email.com",
+            "country": "Australia",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 2,
+            "ballot_id": "001",
+            "name": "Sarah",
+            "address": "Work",
+            "phone_number": "222",
+            "email": "sarah@email.com",
+            "country": "USA",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 3,
+            "ballot_id": "001",
+            "name": "Kenji",
+            "address": "Apartment 1B",
+            "phone_number": "333",
+            "email": "kenji@email.com",
+            "country": "Japan",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 4,
+            "ballot_id": "001",
+            "name": "Maria",
+            "address": "Unit 20",
+            "phone_number": "444",
+            "email": "maria@email.com",
+            "country": "Mexico",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 5,
+            "ballot_id": "001",
+            "name": "David",
+            "address": "Cottage",
+            "phone_number": "555",
+            "email": "david@email.com",
+            "country": "Canada",
+            "application_status": "REJECTED"
+        },
+        {
+            "id": 6,
+            "ballot_id": "001",
+            "name": "Fatima",
+            "address": "Farmhouse",
+            "phone_number": "666",
+            "email": "fatima@email.com",
+            "country": "UAE",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 7,
+            "ballot_id": "001",
+            "name": "Liam",
+            "address": "Dorm 3A",
+            "phone_number": "777",
+            "email": "liam@email.com",
+            "country": "Ireland",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 8,
+            "ballot_id": "001",
+            "name": "Sofia",
+            "address": "City Tower",
+            "phone_number": "888",
+            "email": "sofia@email.com",
+            "country": "Brazil",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 9,
+            "ballot_id": "001",
+            "name": "Javier",
+            "address": "Suburbia",
+            "phone_number": "999",
+            "email": "javier@email.com",
+            "country": "Spain",
+            "application_status": "REJECTED"
+        },
+        {
+            "id": 10,
+            "ballot_id": "001",
+            "name": "Chloe",
+            "address": "Penthouse",
+            "phone_number": "000",
+            "email": "chloe@email.com",
+            "country": "France",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 11,
+            "ballot_id": "001",
+            "name": "Wei",
+            "address": "No. 1 Street",
+            "phone_number": "101",
+            "email": "wei@email.com",
+            "country": "China",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 12,
+            "ballot_id": "001",
+            "name": "Hans",
+            "address": "Main Road 5",
+            "phone_number": "102",
+            "email": "hans@email.com",
+            "country": "Germany",
+            "application_status": "PENDING"
+        },
+        {
+            "id": 13,
+            "ballot_id": "001",
+            "name": "Aisha",
+            "address": "The Coast",
+            "phone_number": "103",
+            "email": "aisha@email.com",
+            "country": "Nigeria",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 5,
+            "ballot_id": "001",
+            "name": "David",
+            "address": "Cottage",
+            "phone_number": "555",
+            "email": "david@email.com",
+            "country": "Canada",
+            "application_status": "REJECTED"
+        },
+        {
+            "id": 6,
+            "ballot_id": "001",
+            "name": "Fatima",
+            "address": "Farmhouse",
+            "phone_number": "666",
+            "email": "fatima@email.com",
+            "country": "UAE",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 7,
+            "ballot_id": "001",
+            "name": "Liam",
+            "address": "Dorm 3A",
+            "phone_number": "777",
+            "email": "liam@email.com",
+            "country": "Ireland",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 8,
+            "ballot_id": "001",
+            "name": "Sofia",
+            "address": "City Tower",
+            "phone_number": "888",
+            "email": "sofia@email.com",
+            "country": "Brazil",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 9,
+            "ballot_id": "001",
+            "name": "Javier",
+            "address": "Suburbia",
+            "phone_number": "999",
+            "email": "javier@email.com",
+            "country": "Spain",
+            "application_status": "REJECTED"
+        },
+        {
+            "id": 10,
+            "ballot_id": "001",
+            "name": "Chloe",
+            "address": "Penthouse",
+            "phone_number": "000",
+            "email": "chloe@email.com",
+            "country": "France",
+            "application_status": "APPROVED"
+        },
+        {
+            "id": 11,
+            "ballot_id": "001",
+            "name": "Wei",
+            "address": "No. 1 Street",
+            "phone_number": "101",
+            "email": "wei@email.com",
+            "country": "China",
+            "application_status": "SUBMITTED"
+        },
+        {
+            "id": 12,
+            "ballot_id": "001",
+            "name": "Hans",
+            "address": "Main Road 5",
+            "phone_number": "102",
+            "email": "hans@email.com",
+            "country": "Germany",
+            "application_status": "PENDING"
+        },
+        {
+            "id": 13,
+            "ballot_id": "001",
+            "name": "Aisha",
+            "address": "The Coast",
+            "phone_number": "103",
+            "email": "aisha@email.com",
+            "country": "Nigeria",
+            "application_status": "APPROVED"
+        }
+    ];
+
+
+
+    const ballotApplications = localData.filter(item => item.ballot_id === ballotId); // Filtering applications based on ballot id
+    const totalPage = Math.ceil(ballotApplications.length / itemsPerPage);
+
+    const currentData = ballotApplications.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+    );
+
+
+    return (
+        <div className="container mx-auto px-4 md:px-6">
+            <div className="relative min-h-[calc(100vh-8rem)] bg-white flex justify-center">
+                <div className="font-inter flex flex-col w-full mt-4">
+                    <h1 className="text-3xl font-bold text-mallorca-purple text-left">{t("admin-dashboard.headline")}</h1>
+                    <div className={"flex justify-between mt-2"}>
+                        <Link to={"/login"} className={"text-green-500"}>{t("admin-dashboard.links.active_requests")}</Link>
+                        <button onClick={handleNewApplicationClick} className={"text-blue-500 underline"}>{t("admin-dashboard.links.ballot_management")}</button>
+                    </div>
+
+                    {/*<button*/}
+                    {/*    type="button"*/}
+                    {/*    className="bg-mallorca-purple text-white px-10 py-2 rounded-md font-medium text-lg mt-12 w-max"*/}
+                    {/*    onClick={handleNewApplicationClick}*/}
+                    {/*>*/}
+                    {/*    {t("dashboard.buttonLabel")}*/}
+                    {/*</button>*/}
+                    <h1>Ballot Id: {ballotId}</h1>
+                    {currentData.length > 0 && (
+                        <div className="mt-16 w-full">
+                            <h1 className="font-bold text-xl text-mallorca-purple">
+                                {t("dashboard.title")}
+                            </h1>
+
+                            <div className="overflow-x-auto">
+                                <table className="mt-8 text-lg dashboard-table text-mallorca-purple">
+                                    <tr>
+                                        <th>{t("admin-dashboard.table.requester_name")}</th>
+                                        <th>{t("admin-dashboard.table.address")}</th>
+                                        <th>{t("admin-dashboard.table.phone_number")}</th>
+                                        <th>{t("admin-dashboard.table.email")}</th>
+                                        <th>{t("admin-dashboard.table.country")}</th>
+                                        <th>{t("admin-dashboard.table.status")}</th>
+                                    </tr>
+
+                                    {currentData.map((item) => (
+                                        <tr key={item.id}>
+                                            <td>{item.name}</td>
+                                            <td>{item.address}</td>
+                                            <td>{formatDate(item.phone_number)}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.country}</td>
+                                            <td>
+                        <span
+                            className={`inline-flex items-center justify-center text-center p-1 px-4 min-w-[14rem] rounded-md ${
+                                // green
+                                ["SELECTED", "PAYMENT_RECEIVED"].includes(
+                                    item.application_status
+                                )
+                                    ? "text-green-800 bg-green-200"
+                                    : // blue
+                                    ["SUBMITTED",
+                                        "UNDER_REVIEW",
+                                        "AWAITING_PAYMENT",
+                                        "APPROVED",
+                                        "IN_BALLOT",]
+                                        .includes(item.application_status)
+                                        ? "text-blue-800 bg-blue-200"
+                                        :
+                                        ["CANCELLED", "REJECTED", "NOT_SELECTED",].includes(
+                                            item.application_status
+                                        )
+                                            ? "text-red-800 bg-red-200"
+                                            : // grey
+                                            ["DRAFT", "EXPIRED",].includes(item.application_status)
+                                                ? "text-gray-800 bg-gray-200"
+                                                : // orange (all in-process)
+                                                ["DOCUMENTS_SUBMITTED", "VERIFICATION_PENDING",].includes(item.application_status)
+                                                    ? "text-orange-800 bg-orange-200"
+                                                    : // fallback
+                                                    "text-mallorca-purple bg-mallorca-purple/10"
+                            }`}
+                        >
+                          {t(
+                              "dashboard.licenceStatus." +
+                              item.application_status
+                                  .toLowerCase()
+                                  .replace(/_([a-z])/g, (_, c) => c.toUpperCase()) // Camel case
+                          )}
+                        </span>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </table>
+
+                                <div className="my-8" />
+
+                                <Pagination
+                                    currentPage={currentPage}
+                                    totalPage={totalPage}
+                                    onPageChange={setCurrentPage}
+                                    start={(currentPage - 1) * itemsPerPage + 1}
+                                    end={currentPage * itemsPerPage}
+                                    numberOfItems={ballotApplications.length}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
--- license-application-service/src/main/webapp/app/routes.tsx
+++ license-application-service/src/main/webapp/app/routes.tsx
@@ -12,6 +12,7 @@ import LegalNotice from "./legal/legal";
 import ContactPage from "./contact/contact";
 import AdminDashboard from "./admin-dashboard/admin-dashboard";
 import BallotDashboard from "./ballot-dashboard/ballot-dashboard";
+import BallotApplicationsDashboard from "./ballot-applications-dashboard/ballot-applications-dashboard";
 
 export default function AppRoutes() {
   const router = createBrowserRouter([
@@ -26,6 +27,7 @@ export default function AppRoutes() {
         { path: "payment", element: <Payment /> },
         { path: "admin-dashboard", element: <AdminDashboard /> },
         { path: "ballot-dashboard", element: <BallotDashboard /> },
+        { path: "ballot-applications-dashboard/:ballotId", element: <BallotApplicationsDashboard /> },
         { path: "legal", element: <LegalNotice /> },
         { path: "contact", element: <ContactPage /> },
         { path: "error", element: <Error /> },
