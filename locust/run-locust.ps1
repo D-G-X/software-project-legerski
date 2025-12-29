a036f8c546eea9bed715af6fcbaea387d9704b36
Jannik Leutgeb <jannik@familie-leutgeb.de>
2025-12-29 19:45:54 +0100

    Move locust files and add setup scripts

---
+++ locust/run-locust.ps1
@@ -0,0 +1,31 @@
+# setup-venv.ps1
+
+$CallerDir = Get-Location
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Set-Location $ScriptDir
+
+if (-not (Test-Path ".venv")) {
+    Write-Host "→ .venv not found. Creating new virtual environment..."
+    python3 -m venv .venv
+}
+
+# Activate virtual environment
+& .\.venv\Scripts\Activate.ps1
+
+python -m pip install --upgrade pip
+
+if (-not (Test-Path "requirements.txt")) {
+    Write-Host "❌ requirements.txt missing"
+    Set-Location $CallerDir
+    exit 1
+}
+
+pip install -r requirements.txt
+
+python3 .\locustfile.py
+
+# Deactivate virtual environment
+deactivate
+
+Set-Location $CallerDir
