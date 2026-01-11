90044297ec184d555a8f31cf6aeaa6a57a2aa83a
Darren Gonsalves <darrengons3001@gmail.com>
2026-01-11 16:34:43 +0100

    Admin Default page Ballot Dashboard

--- license-application-service/src/main/webapp/app/home/home.tsx
+++ license-application-service/src/main/webapp/app/home/home.tsx
@@ -1,30 +1,35 @@
-import React, {useContext} from "react";
-import {useTranslation} from "react-i18next";
+import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
 import useDocumentTitle from "app/common/use-document-title";
-import {AuthContext} from "app/common/AuthContext";
-import {Dashboard} from "app/dashboard/dashboard";
+import { AuthContext } from "app/common/AuthContext";
+import { Dashboard } from "app/dashboard/dashboard";
+import BallotDashboard from "app/ballot-dashboard/ballot-dashboard";
 
 export default function Home() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const auth = useContext(AuthContext);
 
   useDocumentTitle(t("home.index.headline"));
 
   return auth?.accessToken ? (
-      <Dashboard/>
+    auth.role === "admin" ? (
+      <BallotDashboard></BallotDashboard>
+    ) : (
+      <Dashboard />
+    )
   ) : (
-      <main className="relative w-full overflow-hidden">
-        <div
-            className="absolute inset-0 bg-center bg-cover bg-no-repeat opacity-80 scale-110 -z-10"
-            style={{
-              backgroundImage: "url('/images/background.webp')",
-              backgroundPosition: "center bottom",
-            }}
-        />
+    <main className="relative w-full overflow-hidden">
+      <div
+        className="absolute inset-0 bg-center bg-cover bg-no-repeat opacity-80 scale-110 -z-10"
+        style={{
+          backgroundImage: "url('/images/background.webp')",
+          backgroundPosition: "center bottom",
+        }}
+      />
 
-        <div className="relative min-h-[calc(100vh-8rem)] flex items-center justify-center">
-          <h1
-              className="
+      <div className="relative min-h-[calc(100vh-8rem)] flex items-center justify-center">
+        <h1
+          className="
             text-stroke-1
             sm:text-stroke-3
             lg:text-stroke-5
@@ -40,10 +45,10 @@ export default function Home() {
             select-none
             z-10
           "
-          >
-            {t("home.index.headline")}
-          </h1>
-        </div>
-      </main>
+        >
+          {t("home.index.headline")}
+        </h1>
+      </div>
+    </main>
   );
 }
